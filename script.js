002709c6034bf31cbf701789bd4b3a4ecd6af5f4
Robin BOURACHOT <robin.bourachot@hetic.net>
2020-05-04 05:25:03 +0200

    :bug: audio issue fix
    vary clean code, I'm proud

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
     
 //   });
 
-const button = document.querySelectorAll('.but');
 
+const button = document.querySelectorAll('.but');
 for (let but of button) {
 	ggfunct(but)
 }
@@ -13,6 +13,7 @@ for (let but of button) {
 let trim = {}
 
 function ggfunct(but){
+	
 	but.onclick = function() {
 		const dataKey = but.getAttribute('data-key')
 		soundFunct(dataKey)
@@ -26,12 +27,23 @@ function keySelect(event){
 }
 
 
-function soundFunct(dataKey) {
+function soundFunct(dataKey) {	
 	const audio = document.querySelector(`audio[data-key='${dataKey}']`)
 	const trim = document.querySelector(`.but[data-key='${dataKey}']`)
+
+	// console.log(audio);
+	
 	if(!audio) {
 		return;
     }
+	const nameAudio = audio.getAttribute('data-id')
+	
+    document.querySelector(".textTest").innerHTML = nameAudio;
+
+	// textDisplay.textContent = nameAudio
+	console.log(nameAudio);
+	
+	
 	audio.currentTime = 0
     audio.play()
 	trim.classList.add('active')
@@ -42,18 +54,18 @@ function soundFunct(dataKey) {
 
 
 
-var x = window.matchMedia('(max-width: 600px)')
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
+const mediaChange = window.matchMedia('(max-width: 600px)')
+changeKeys(mediaChange) // Call listener function at run time
+mediaChange.addListener(changeKeys) // Attach listener function on state changes
 
 
-function myFunction(x) {
+function changeKeys(mediaChange) {
 	
 	// i really did everything i could but i didn't find a way to get the characters automatically and save them
 	const mem = ['A', 'Z', 'E', 'R', 'T', 'Y', 'Q', 'S', 'D', 'F', 'G', 'H', 'W', 'X', 'C', 'V', 'B', 'N']
 	const y = document.querySelectorAll('.frou');
 	
-	if (x.matches) { // If media query matches
+	if (mediaChange.matches) { // If media query matches
 		var i;
 		for (i = 0; i < y.length; i++) {
 			y[i].innerHTML = '•';
